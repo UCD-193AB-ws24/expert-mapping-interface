96de3b0db4ae722f2ab07b5f935dfc14fff18453
marinamata <mmbmata@ucdavis.edu>
2025-04-23 14:22:17 -0700

    MapContainer documented

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,24 +1,37 @@
 
-// components/map/MapContainer.js
 
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+/**
+ * MapWrapper Component
+ * 
+ * This component serves as a wrapper for the Leaflet map. It provides a base map with configurable
+ * settings such as initial view, zoom levels, and map bounds. It also allows child components to
+ * be rendered within the map, enabling the addition of layers, markers, and other map features.
+ * 
+ * Features:
+ * - Sets the initial center and zoom level of the map.
+ * - Restricts zoom levels and map bounds to prevent users from panning too far.
+ * - Includes a TileLayer for rendering the base map using OpenStreetMap tiles.
+ */
+
 const MapWrapper = ({ children }) => {
   return (
     <MapContainer
-      center={[30, 0]} // initial view
-      zoom={2}
-      minZoom={2}
-      maxZoom={4}
-      style={{ height: "100%", width: "100%" }}
+      center={[30, 0]} // Initial center of the map [latitude, longitude].
+      zoom={2} // Initial zoom level.
+      minZoom={2} // Minimum zoom level to prevent excessive zooming out.
+      maxZoom={4} // Maximum zoom level to prevent excessive zooming in.
+      style={{ height: "100%", width: "100%" }} // Full height and width to fit the container.
       maxBounds={[
-        [-80, -200], // Southwest corner
-        [85, 200]    // Northeast corner
+        [-80, -200], // Southwest corner of the map bounds
+        [85, 200]    // Northeast corner of the map bounds
       ]}
-      maxBoundsViscosity={1.0}
+      maxBoundsViscosity={1.0} // Viscosity for the bounds to prevent panning outside the defined area.
     >
+      {/* Add a TileLayer to render the base map using OpenStreetMap tiles. */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
