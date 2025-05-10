6826f0e24319032e8916174e9124a0b765ba5d4b
marinamata <mmbmata@ucdavis.edu>
2025-05-10 08:37:12 -0700

    frontend README files

--- src/styles/icon-fix.js
+++ src/styles/icon-fix.js
@@ -1,12 +1,28 @@
+/**
+ * @file icon-fix.js
+ * @description This file fixes the default marker icon issue in Leaflet by explicitly setting the default marker icon
+ *              and shadow images. This ensures that the Leaflet markers are displayed correctly when using the library
+ *              in a React application or a build environment where the default assets might not be resolved properly.
+ *
+ * Features:
+ * - Imports the default marker icon and shadow images from the Leaflet package.
+ * - Configures the default Leaflet marker icon with appropriate size and anchor settings.
+ * - Applies the custom default icon to all Leaflet markers globally.
+ *
+ * Marina Mata, 2025
+ */
+
 import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+// Define the default Leaflet marker icon
 let DefaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41]
+    iconSize: [25, 41], // Size of the icon
+    iconAnchor: [12, 41] // Anchor point of the icon
 });
 
-L.Marker.prototype.options.icon = DefaultIcon; 
+// Set the default icon for all Leaflet markers
+L.Marker.prototype.options.icon = DefaultIcon;
